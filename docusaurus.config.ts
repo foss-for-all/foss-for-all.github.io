56949436ae3f86dde2e7471bbaaa7fe0719d5554
Youngbin Han <ybhan@ubuntu.com>
2025-02-27 00:08:43 +0900

    랜딩 페이지 배너 수정 및 i18n 적용

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,20 +5,20 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'FOSS for All',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://foss-for-all.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'foss-for-all', // Usually your GitHub org/user name.
+  projectName: 'foss-for-all.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -27,8 +27,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ko',
+    locales: ['ko', 'en'],
   },
 
   presets: [
@@ -68,7 +68,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'FOSS for All',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -84,6 +84,10 @@ const config: Config = {
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
           position: 'right',
         },
       ],
@@ -131,7 +135,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} FOSS for All. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
+import Translate, {translate} from '@docusaurus/Translate';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import OpenSourceIcon from '@site/static/img/opensource.svg';
+import PeopleIcon from '@site/static/img/people.svg';
+import LeafIcon from '@site/static/img/leaf.svg';
+import HeartIcon from '@site/static/img/hand-heart.svg';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -16,14 +21,14 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <p className="hero__title"><Translate id='homepage.heroBanner.tagLine'>지속 가능한 자유-오픈소스 소프트웨어 생태계 구축을 위한 이니셔티브</Translate></p>
+        
+      </div>
+      <div className="container images" style={{ textAlign: 'center'}}>
+        <OpenSourceIcon height={200}/>
+        <PeopleIcon height={200}/><br/>
+        <LeafIcon height={200}/>
+        <HeartIcon height={200}/>
       </div>
     </header>
   );
