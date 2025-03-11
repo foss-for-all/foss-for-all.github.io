a038f066e0b2d9a5bb3082322a5ae6c906d918b3
Youngbin Han <ybhan@ubuntu.com>
2025-03-11 23:49:25 +0900

    푸터에 소스코드 링크 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -80,12 +80,7 @@ const config: Config = {
         //   position: 'left',
         //   label: '문서',
         // },
-        {to: '/blog', label: '소식/공고', position: 'left'},
-        {
-          href: 'https://github.com/foss-for-all/foss-for-all.github.io',
-          label: 'GitHub',
-          position: 'right',
-        },
+        { to: '/blog', label: '소식/공고', position: 'left' },
         {
           type: 'docSidebar',
           sidebarId: 'disclosuresSidebar',
@@ -115,6 +110,10 @@ const config: Config = {
             {
               label: 'GitHub',
               href: 'https://github.com/foss-for-all/',
+            },
+            {
+              label: 'Source code for this website',
+              href: 'https://github.com/foss-for-all/foss-for-all.github.io',
             },
           ],
         },
